0f3993550516727a7fdaa86b13d9624ac8cf6ef8
iago159 <71942285+iago159@users.noreply.github.com>
2021-06-26 19:35:53 -0300

    Project finished
    Uploaded all python files

---
+++ FlaskMarket/dbcon.py
@@ -0,0 +1,9 @@
+from market import app
+from flask_mysqldb import MySQL
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'market'
+
+mysql = MySQL(app)
